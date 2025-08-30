8b84f846cdd28cd4d4c8ee6655dcd506cc2ec559
Caylusis2 <caylusis2@yahoo.com>
2025-08-30 15:35:19 +0800

    Update testquestion1.js

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,26 @@
+function setup(){
+    createCanvas(600,400);
+    background(10);
+}
+
+function draw(){
+    fill("black");
+    stroke("red");
+    strokeWeight(10);
+    circle(150,200,100);
+
+    
+    fill("black");
+    stroke("red");
+    strokeWeight(10);
+    triangle(240,250,360,250,300,150);
+
+    fill("black");
+    stroke("red");
+    strokeWeight(10);
+    rect(430,150,100,100)
+}
+
+
+
 
